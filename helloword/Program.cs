80adeb232432978b1276fb3fd26e710460333f35
kevin <kevinli@oitc.com.tw>
2022-08-29 16:59:27 +0800

    加入專案檔案。

---
+++ helloword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stry = "",xx="";
+            string strx = " Your name： \n";
+
+            string s = "Hello";
+
+            Console.Write("Please enter");
+            Console.Write($"{strx}");
+
+            stry = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine(" {0},{1} ", s, stry);
+
+            Console.WriteLine("按任意鍵離開");
+            Console.ReadKey();
+
+        }
+    }
+}
